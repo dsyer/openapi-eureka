7fd18b3603a9d25deaed0ac8730b4af21b207fe7
Dave Syer <dsyer@vmware.com>
2024-07-10 10:22:30 +0100

    Update to latest Boot

--- src/main/java/com/example/eurekademo/EurekaDemoApplication.java
+++ src/main/java/com/example/eurekademo/EurekaDemoApplication.java
@@ -2,31 +2,14 @@ package com.example.eurekademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.discovery.shared.Applications;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaDemoApplication.class, args);
-	}
-
-}
-
-@RestController
-@RequestMapping("/{version}")
-class AppsController {
-
-	@GetMapping("/apps")
-	Applications applications(@PathVariable String version, @RequestParam String regions, @RequestHeader("X-Eureka-Accept") String eurekaAccept) {
-		return null;
 	}
 
 }
